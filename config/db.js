970bf0ad11b2812c83d1708c6ed1123bece3b8e8
unknown <sastraharapangulo2002@gmail.com>
2025-05-19 15:34:38 +0700

    update database

--- config/db.js
+++ config/db.js
@@ -3,11 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new pg.Pool({
+  connectionString: process.env.DB_URL,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, // Tambahkan ini jika pakai Railway
+  },
 });
 
 export default pool;
